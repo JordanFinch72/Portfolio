0e93f3f8d775ca77cb6a47344edd25dd7aa6bf34
Jordan <jordan.e.finch@gmail.com>
2023-07-09 17:24:34 +0100

    Main, footer, and Playground styling

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta name="theme-color" content="#FFCC00" />
 	<!-- TODO: Metadata -->
 	<link rel="stylesheet" type="text/css" href="resources/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="resources/css/playground.css" />
 	<title>Jordan Elliot Finch's Portfolio</title>
 </head>
 <body>
@@ -17,12 +18,16 @@
 	<main class="playground">
 		<section>
 			<div class="widget-row">
-				<img src="#" alt="Jordan and Marcie" />
+				<img src="https://placekitten.com/300/250" alt="Jordan and Marcie" />
 			</div>
 			<div class="info-box">
 				<div class="title">My name is Jordan Elliot Finch.</div>
 				<div class="content">
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor sollicitudin velit eget volutpat. Suspendisse a ultrices odio. Mauris eget sem felis. Mauris sollicitudin, elit ac rutrum facilisis, odio enim fringilla diam, nec congue leo libero in neque. Nullam sit amet posuere quam. In tempus aliquet faucibus. Ut at nisl efficitur, convallis libero nec, imperdiet dolor. Mauris volutpat ante ut odio tempus facilisis. Donec et cursus elit, sit amet placerat tellus.
+					<p>
+						I'm a full-stack developer residing in England. I have a range of experience with various frameworks and tech stacks and currently work as a Ruby on Rails developer.
+						<br/><br/>
+						Feel free to take a look through my portfolio and check out the projects I've worked on in the past!
+					</p>
 				</div>
 			</div>
 		</section>

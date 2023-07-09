91fbcc72c2f0f7945e96e62c1340ea82ca952d40
Jordan <jordan.e.finch@gmail.com>
2023-07-09 20:29:59 +0100

    Add personal details page

---
+++ about.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="theme-color" content="#FFCC00" /> <!-- TODO: Check that this doesn't look abysmal -->
+	<!-- TODO: Metadata: descriptions, etc. -->
+	<link rel="stylesheet" type="text/css" href="resources/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="resources/css/alternating.css" />
+	<title>Jordan Elliot Finch - Details</title>
+</head>
+<body>
+	<?php require("modules/header.php") ?>
+
+	<div class="title-header">
+		<h2>Personal Details</h2>
+	</div>
+
+	<main class="alternating">
+		<section>
+			<div class="info-box">
+				<div class="title">Overview</div>
+				<div class="content">
+					<p>
+						Name: Jordan Elliot Finch <br />
+						Age: 26 (28/10/1996) <br />
+						Residence: Bath, England <br />
+						CV: <a href="resources/files/Curriculum Vitae.docx">Download (.docx)</a> | <a href="resources/files/Curriculum Vitae.odt">(.odt)</a> | <a href="resources/files/Curriculum Vitae.pdf" target="_blank">(.pdf)</a>
+					</p>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="info-box">
+				<div class="title">Employment Details</div>
+				<div class="content">
+					<div class="item-wrapper">
+						<strong>Storm Consultancy</strong> — Junior Rails Developer (2022 — present)<br />
+						<p>
+							I currently work for <a href="https://stormconsultancy.co.uk" target="_blank">Storm Consultancy</a>, a web development agency that specialises in making bespokse Ruby on Rails web apps in addition to WordPress sites. I am one of a team of Ruby on Rails developers. My day-to-day involves working on a given project, often interacting with clients, considering their needs, and delivering them the solution that they require.
+						</p>
+					</div>
+					<div class="item-wrapper">
+						<strong>GamePoint B.V.</strong> — Junior Front-end Developer (2015)<br />
+						<p>
+							I worked at this company from June to July 2015, during my time in the Netherlands. The company is an
+							<a href="http://gamepoint.com" target="_blank">online games company</a> based in The Hague. I was responsible for front-end work,
+							including the maintenance and upgrade of some company-side tools.
+						</p>
+					</div>
+					<div class="item-wrapper">
+						<strong>Blueloop Ltd.</strong> — IT Technical Resource (2016)<br />
+						<p>
+							I worked at this company from December 2015 to February 2016. I was contracted to Great Western Railway's IT
+							department. My role included setting up employee laptops to custom specifications and personally handing over
+							the hardware to the user with a thorough demonstration of how to use the hardware. I was also responsible for
+							solving any IT-related issues that employees had.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section>
+			<div class="info-box">
+				<div class="title">Education</div>
+				<div class="content">
+					<div class="item-wrapper">
+						<strong>University of Plymouth</strong> (2017 — 2022)<br />
+						<p>
+							BSc Computing and Software Development: 1st-class <br />
+						</p>
+					</div>
+					<div class="item-wrapper">
+						<strong>Yeovil College</strong> (2013 — 2014)<br />
+						<p>
+							A2 Computing: B <br />
+							AS Computing: A <br />
+							AS Psychology: A <br />
+							AS Sociology: C
+						</p>
+					</div>
+					<div class="item-wrapper">
+						<strong>Languages</strong>
+						<p>
+							English: Native language <br />
+							Dutch: B1/B2-niveau (zelf geleerd)
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+
+	<?php require("modules/footer.php"); ?>
+</body>
+</html>
